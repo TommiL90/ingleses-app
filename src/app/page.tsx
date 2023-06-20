3647336c316e48943112b7c7f7c8a21d9fcb18fb
TBenjaminL90 <tomasbenjamin.l90@gmail.com>
2023-06-19 23:10:03 -0300

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import CardComponent from "@/components/Card";
 import { territories } from "@/constants/territories";
 
+const teste = async () => {
+  try {
+    const res = await fetch(`${process.env.BASE_URL}/api/hello`);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export default async function Home() {
-  const res = await fetch(`${process.env.BASE_URL}/api/hello`);
-  console.log(await res.json());
+  await teste();
   return (
     <main className="container h-screen py-8">
       <ul className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-8">
