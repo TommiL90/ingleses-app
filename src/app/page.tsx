190a31f85b3937035a75e764593de0f9e4fb7e76
TBenjaminL90 <tomasbenjamin.l90@gmail.com>
2023-06-19 22:51:38 -0300

    add baseURL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import CardComponent from "@/components/Card";
 import { territories } from "@/constants/territories";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch(`${process.env.BASE_URL}/api/hello`);
+  console.log();
   return (
     <main className="container h-screen py-8">
       <ul className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-8">
@@ -9,6 +11,7 @@ export default function Home() {
           <CardComponent key={territory.id} territory={territory} />
         ))}
       </ul>
+      {await res.json()}
     </main>
   );
 }
