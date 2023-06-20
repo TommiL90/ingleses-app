3cd430b2d1235de14dfcee451b5fe998b3a41964
TBenjaminL90 <tomasbenjamin.l90@gmail.com>
2023-06-19 22:59:37 -0300

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { territories } from "@/constants/territories";
 
 export default async function Home() {
   const res = await fetch(`${process.env.BASE_URL}/api/hello`);
-  console.log();
+  console.log(await res.json());
   return (
     <main className="container h-screen py-8">
       <ul className="grid grid-cols-[repeat(auto-fit,minmax(320px,1fr))] gap-8">
@@ -11,7 +11,6 @@ export default async function Home() {
           <CardComponent key={territory.id} territory={territory} />
         ))}
       </ul>
-      {await res.json()}
     </main>
   );
 }
