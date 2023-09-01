69c4a4d5232cd433114fd7b3517326bd784d6b8c
Tomas Lillo Sanhueza <108248835+TommiL90@users.noreply.github.com>
2023-09-01 10:53:00 -0300

    feat: add some territories

--- src/constants/maps.ts
+++ src/constants/maps.ts
@@ -6,5 +6,53 @@ export const maps: IGooglemaps[] = [
     title: "Territorio 1",
     neighborhood: "Ingleses",
     url: "https://www.google.com/maps/d/embed?mid=1FU8rwIVXhj2UIXbWlK6o8SPvZIFckBE&ehbc=2E312F"
+  },
+  {
+    id: 2,
+    title: "Territorio 2",
+    neighborhood: "Ingleses",
+    url: "https://www.google.com/maps/d/embed?mid=14RgWwvf9rDs8erDdzte53I5OpANycwI&ehbc=2E312F"
+  },
+  {
+    id: 3,
+    title: "Territorio 3",
+    neighborhood: "Ingleses",
+    url: "https://www.google.com/maps/d/embed?mid=1v11rY8PWXMADb2unaJTTD2oNQfdnyng&ehbc=2E312F"
+  },
+  {
+    id: 4,
+    title: "Territorio 4",
+    neighborhood: "Ingleses",
+    url: "https://www.google.com/maps/d/embed?mid=1sdmveitsGC_NgUNSFMe-3XKVcUPGOug&ehbc=2E312F"
+  },
+  {
+    id: 5,
+    title: "Territorio 5",
+    neighborhood: "Ingleses",
+    url: "https://www.google.com/maps/d/embed?mid=1n_492hwdtB3oZpkOJPVjocbaV4JO-5I&ehbc=2E312F"
+  },
+  {
+    id: 6,
+    title: "Territorio 6",
+    neighborhood: "Ingleses",
+    url: "https://www.google.com/maps/d/embed?mid=116lTcIXkuv90CTCddXj9TcZIfE9HCYE&ehbc=2E312F"
+  },
+  {
+    id: 19,
+    title: "Territorio 19",
+    neighborhood: "Santinho",
+    url: "https://www.google.com/maps/d/embed?mid=1tIhN8rXecIkpIWvdshWTks14PHi6Vg8&ehbc=2E312F"
+  },
+  {
+    id: 20,
+    title: "Territorio 20",
+    neighborhood: "Santinho",
+    url: "https://www.google.com/maps/d/embed?mid=1SOw5YEJjdTKLLOeFGnfeWpj80FUtfcM&ehbc=2E312F"
+  },
+  {
+    id: 29,
+    title: "Territorio 29",
+    neighborhood: "Rio Vermelho",
+    url: "https://www.google.com/maps/d/embed?mid=1jds-sn7wiQeGi-3sXlPKbmgObbpI4F8&ehbc=2E312F"
   }
 ];
