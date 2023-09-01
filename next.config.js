9d6e3dcfbf3515eafd3a4fdc5f36d21f06023f53
Tomas Lillo Sanhueza <108248835+TommiL90@users.noreply.github.com>
2023-09-01 10:18:46 -0300

    feat: add `PWA`

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: process.env.NODE_ENV !== "development"
+};
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true
+});
+
+export default withPWA(nextConfig);
